ca913976518fb867625e2223fe4f0ef204854c5b
Tollef Fog Heen <tfheen@err.no>
2011-05-25 13:54:42 +0200

    Typo, add more supports

--- varnish/metadata.rb
+++ varnish/metadata.rb
@@ -1,9 +1,9 @@
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
-description       "Instsalls and configures varnish"
+description       "Installs and configures varnish"
 version           "0.7"
 
-%w{ubuntu debian}.each do |os|
+%w{ubuntu debian centos}.each do |os|
   supports os
 end
