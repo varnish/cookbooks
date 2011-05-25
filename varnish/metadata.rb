3d5e2173fa31bc5bc3df8ff49e5a03e0f5635470
Tollef Fog Heen <tfheen@err.no>
2011-05-25 13:56:16 +0200

    More metadata updates

--- varnish/metadata.rb
+++ varnish/metadata.rb
@@ -1,5 +1,5 @@
-maintainer        "Opscode, Inc."
-maintainer_email  "cookbooks@opscode.com"
+maintainer        "Varnish Software"
+maintainer_email  "tfheen@varnish-software.com"
 license           "Apache 2.0"
 description       "Installs and configures varnish"
 version           "0.7"
